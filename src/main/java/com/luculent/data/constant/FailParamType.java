0953fdb7d8dfabc32409f8579de57a46bfd6e9b4
zhangy <17909328@163.com>
2017-05-06 08:59:29 +0800

    返回失败参数值

---
+++ src/main/java/com/luculent/data/constant/FailParamType.java
@@ -0,0 +1,17 @@
+package com.luculent.data.constant;
+
+public enum FailParamType {
+    NETWORK("网络连接不稳定"),LOGIN("自动登陆失败"),NOEXISTS("参数不存在");
+    
+    private String reason;
+    
+    private FailParamType(String reason) {
+	// TODO Auto-generated constructor stub
+	this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+}
