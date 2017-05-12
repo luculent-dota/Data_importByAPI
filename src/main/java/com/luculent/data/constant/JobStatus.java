d8c1f542e75c1fe44b2343a3fd0ae2d93ec5e174
zhangy <17909328@163.com>
2017-05-12 14:34:41 +0800

    更改任务状态常量

--- src/main/java/com/luculent/data/constant/JobStatus.java
+++ src/main/java/com/luculent/data/constant/JobStatus.java
@@ -1,17 +1,22 @@
 package com.luculent.data.constant;
 
-public enum JobStatus {
+import com.luculent.data.base.IntegerEnum;
+
+public enum JobStatus implements IntegerEnum{
     ENABLED(1),DISABLED(0);
     
-    private int val;
+    private Integer val;
     
-    private JobStatus(int val) {
+    private JobStatus(Integer val) {
 	// TODO Auto-generated constructor stub
 	this.val = val;
     }
 
-    public int getVal() {
-        return val;
+    @Override
+    public Integer getVal() {
+	// TODO Auto-generated method stub
+	return this.val;
     }
+
     
 }
